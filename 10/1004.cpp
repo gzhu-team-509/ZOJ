603abdbf41ce7a0ec898f68d8dc1fa1af8ba8942
lightyears1998 <lightyears1998@hotmail.com>
2017-11-14 10:31:02 +0800

    11.14

--- 10/1004.cpp
+++ 10/1004.cpp
@@ -1,1 +1,57 @@
-// 不懂BFS，弃坑
+/* Anagrams by Stack */
+/* 《算法设计与分析》P25 */
+#include <iostream>
+#include <string>
+#include <stack>
+#include <vector>
+using namespace std;
+
+string a, b;
+vector<char> opearate;
+stack<char> build;
+int length;
+
+void dfs(int iPush, int iPop)
+{
+    // 打印成功构造目标单词时的操作序列
+    if (iPush == length && iPop == length)
+    {
+        for (int i = 0; i < length * 2; i++)
+        {
+            cout << opearate[i] << " ";
+        }
+        cout << endl;
+    }
+
+    // 入栈操作
+    if (iPush + 1 <= length)
+    {
+        build.push(a[iPush]);
+        opearate.push_back('i');
+        dfs(iPush + 1, iPop);
+        build.pop();
+        opearate.pop_back();
+    }
+
+    // 出栈操作
+    if (iPop + 1 <= iPush && build.top() == b[iPop])
+    {
+        char c = build.top();
+        build.pop();
+        opearate.push_back('o');
+        dfs(iPush, iPop + 1);
+        build.push(c);
+        opearate.pop_back();
+    }
+}
+
+int main()
+{
+    while (cin >> a >> b)
+    {
+        length = a.length();
+        cout << '[' << endl;
+        dfs(0, 0);
+        cout << ']' << endl;
+    }
+}
